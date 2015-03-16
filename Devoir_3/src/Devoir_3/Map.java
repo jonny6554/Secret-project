4b93e29295054b2ecf41352bc9abb3574d27b974
Jonathan Horton <astuteportia@gmail.com>
2015-03-16 19:36:56 -0400

    Revert "J'ai mit des commentaires dans Map.java."
    This reverts commit 72b9b23c3dee610755df8976175cfdd96c587f74.

--- Devoir_3/src/Devoir_3/Map.java
+++ Devoir_3/src/Devoir_3/Map.java
@@ -14,50 +14,13 @@ import java.util.NoSuchElementException;
  */
 public interface Map<K,V> {
 	
-	/**
-	 * Retourne la valeur associé à la clé spécifiée.
-	 * Dans le cas d'un conflit, elle retourne la valeur la plus à gauche.
-	 * 
-	 * @param key la clé est associé à une valeur.
-	 * @return on retourne la valeur associé à la clé donnée
-	 * @throws NoSuchElementException est généré lorsqu'aucun valeur est associé à la clé.
-	 * @throws NullPointerException est généré lorsque la clé spécifié est null.
-	 */
 	public abstract V get(K key)throws NoSuchElementException, NullPointerException;
 	
-	/**
-	 * Retourne true si une association existe pour la clé spécifiée.
-	 * 
-	 * @param key est la clé est associé à une valeur.
-	 * @return est vrai seulement s'il existe une association pour la clé donnée
-	 */
 	public abstract boolean contains(K key);
 	
-	/**
-	 * Crée une nouvelle association clé-valeur.
-	 * 
-	 * @param key est la clé qui sera associé à la valeur donnée
-	 * @param value est la valeur qui sera associé à la clé donné.
-	 * @throws NullPointerException
-	 */
 	public abstract void put(K key, V value) throws NullPointerException;
 	
-	/**
-	 * Cette méthode remplace la valeur associé au clé spécifié.
-	 * Dans le cas d'un conflit, elle remplace la valeur la plus à gauche.
-	 * 
-	 * @param key est la clé pour laquelle la valeur sera remplacé
-	 * @param value est la nouvelle valeur à associé au clé.
-	 * @throws NoSuchElementException est généré lorsqu'il n'y a pas de clé existent que l'on peut remplacer.
-	 */
 	public abstract void replace(K key, V value) throws NoSuchElementException;
 	
-	/**
-	 * Cette méthode permet d'enlevé la valeur associé à une clé.
-	 * Dans le cas d'un conflit, elle enlève la valeur la plus à gauche.
-	 * 
-	 * @param key est la clé pour laquelle l'association sera enlevé.
-	 * @return la valeur qui vient d'être enlevé.
-	 */
 	public abstract  V remove(K key);
 }
