e179306fde695287cdde9228875cfd426cf306a8
matem6554 <matem12345@hotmail.fr>
2015-03-16 18:28:33 -0400

    Test

--- Devoir_3/src/Devoir_3/StudentInfo.java
+++ Devoir_3/src/Devoir_3/StudentInfo.java
@@ -6,27 +6,31 @@ package Devoir_3;
 
 public class StudentInfo {
 
-    /**
-     * Displays the student information: student name, id, section, etc. You
-     * must fill this information inside the box.
-     */
-    public static void display() {//TEst2
+	/**
+	 * Displays the student information: student name, id, section, etc. You
+	 * must fill this information inside the box.
+	 */
+	public static void display() {// TEst2
 
-        System.out
-                .println("************************************************************");
-        System.out
-                .println("*         Jonathan Horton et Mathieu Harter                *");
-        System.out
-                .println("*          7710257        et      7377180                  *");
-        System.out
-                .println("*          		Laboratoire 1 et Laboratoire 2         	 *");//Test push
-        System.out
-                .println("*           ITI 1521A (PROF: NATHALIE JAPKOWICZ)           *");
-                .println("*           ITI 1521A (PROF: NATHALIE JAPKOWICZ)           *");//Make change. Again.
-        System.out
-                .println("************************************************************");
-        System.out.println();
+		System.out
+				.println("************************************************************");
+		System.out
+				.println("*         Jonathan Horton et Mathieu Harter                *");
+		System.out
+				.println("*          7710257        et      7377180                  *");
+		System.out
+				.println("*          		Laboratoire 1 et Laboratoire 2         	 *");// Test
+																					// push
+		System.out
+				.println("*           ITI 1521A (PROF: NATHALIE JAPKOWICZ)           *");
+		System.out
+				.println("*           ITI 1521A (PROF: NATHALIE JAPKOWICZ)           *");// Make
+																							// change.
+																							// Again.
+		System.out
+				.println("************************************************************");
+		System.out.println();
 
-    }
+	}
 
 }
