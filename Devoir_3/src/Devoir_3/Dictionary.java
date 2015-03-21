e3be3c3cd0945bba950c1ba277d422a9f1e47f70
Jonathan Horton <astuteportia@gmail.com>
2015-03-21 10:52:53 -0400

    J'ai presque complété la classe Dictionary.java. J'ai fini la méthode remove, et je retravaillé les méthodes get, contains, put et replace. Le seul problème que je rancontre toujours dans DictionaryTest.java est celui des test d'erreurs.
    La raison pour laquelle la classe échoue ces tests est dû au fait que
    @Test(expected = X.class) ne retourne qu'un "pass" si l'on n'attrape pas
    l'erreur. Alors, il faudrait enlevé les "try" et "catch" si l'on veut
    passer tout les tests de DictionaryTest. Je suis entrain de communiqué
    avec le professeur à cette égard.
    
    Autrement, la classe est fonctionnel.

--- Devoir_3/src/Devoir_3/Dictionary.java
+++ Devoir_3/src/Devoir_3/Dictionary.java
@@ -2,12 +2,30 @@ package Devoir_3;
 
 import java.util.NoSuchElementException;
 
+/**
+ * Une instance de Dictionary contient des pairs d'éléments clef-valeur (key-value)
+ * qui peuvent être manipulé avec les méthodes de la classe. Un map peut contenir des
+ * associations clef-valeur dupliqués. Lorsque c'est le cas, au besoin, la méthode 
+ * appelée fait référence à la dernière valeur pour une clef donnée. 
+ * 
+ * Un dictionnaire est analogue à une matrice de variables: chaque variable a un nom
+ * (la clef) et chaque variable est associé à une valeur (valeur de type <code> Token </code> 
+ * (<code> String </code> ou <code> int </code>))
+ * 
+ * @author Jonathan Horton (jhort062@uottawa.ca) et Mathieu Harter (matem12345@hotmail.fr)
+ * @version sqrt(1.0)
+ */
 public class Dictionary implements Map<String, Token> {
 
 	/**
 	 * L'atribut dictionnaire contient les paires d'associatoins.
 	 */
 	private Pair[] dictionary;
+	
+	/**
+	 * Indique le nombre d'éléments dans le dictionnaire.
+	 */
+	private int numberOfElements;
 
 	/**
 	 * Contient la valeur pour initialisé l'attribut
@@ -20,169 +38,215 @@ public class Dictionary implements Map<String, Token> {
 	private static final int SIZE_ADDED_TO_FULL_DICTIONARY = 5;
 
 	/**
-	 * Le constructeur de dictionnaire; elle initialise le paramètre
-	 * dictionnaire.
+	 * Le constructeur de <code> dictionnaire </code> et <code> numberOfElements; 
+	 * elle initialise les paramètres. Elle créé aussi de nouvelles instances de Pair 
+	 * dans le dictionnaire. Par défaut, la longueur initiale du dictionnaire
+	 * est indiqué par <code> INITIAL_SIZE_OF_DICTIONARY </code>.
 	 */
 	public Dictionary() {
+		numberOfElements = 0;
 		dictionary = new Pair[INITIAL_SIZE_OF_DICTIONARY];
 		// Pair est statique; il n'est pas initialiser.
+		for (int i = 0; i < dictionary.length; i++)
+			dictionary[i] = new Pair();
 	}
 
 	@Override
 	/**
-	 * Retourne la valeur associé à la clé spécifiée.
+	 * Retourne la valeur associé à la clef spécifiée.
 	 * Dans le cas d'un conflit, elle retourne la valeur la plus à gauche.
 	 * 
 	 *
 	 * @param key : symbole lu dans le programme de Luka
 	 * @throws NoSuchElementException : lance l'exception si le token recherché n'existe pas
 	 * @throws NullPointerException : lance l'exception si le key est null
-	 * @return
+	 * @return Retourne la valeur du Token associé au clef
 	 */
 	public Token get(String key) throws NoSuchElementException,
 			NullPointerException {
+		//Déclaration des variables et dictionnaire des données
+		Token resultat = null; //Le résultat de la méthode.
+		boolean found =false; //true seulement si l'élément recherché a été trouvé
+		
 		// Module
 		try {
-			for (Pair i : dictionary)
-				if (key != null)
-					if (i.key.toString() == key)
-						if (i.value != null)
-							return i.value;
-						else
-							// La valeur est nul, alors on lance une exception.
-							throw new NullPointerException();
-			// Car la clé n'as pas été trouvé.
-			throw new NoSuchElementException();
+			
+			for (int i = numberOfElements-1; i >= 0; i--)
+				if (key != null){
+					if (dictionary[i].key.equals(key)){
+							resultat = dictionary[i].value;
+							found = true;
+							break;
+						}
+				}
+				else 	// La valeur est nul, alors on lance une exception.
+					throw new NullPointerException();
+			if(!found)
+				throw new NoSuchElementException();
 
 		} catch (NoSuchElementException e1) {
-			System.out
-					.println("L'élément demandé n'existe pas; il n'y avait pas de token!");
+			System.out.println("L'élément demandé n'existe pas; la clef est null. La méthode: get. "
+							+ "Contenu de la clef: " + key);
 		} catch (NullPointerException e2) {
-			System.out.println("Il n'avait aucun élément associé au clé!.");
+			System.out.println("Il n'avait aucun élément associé au clef! La méthode: get. "
+					+ "Contenu de la clef:" + key);
 		}
-		// Pour la compilation
-		return null;
+		
+		//Retour de la variable résultat
+		return resultat;
 	}
 
 	@Override
 	/**
-	 * Retourne true si une association existe pour la clé spécifiée.
+	 * Retourne true si une association existe pour la clef spécifiée.
 	 * 
 	 * @param String : symbole lu dans le programme de Luka
-	 * @throws NullPointerException est généré lorsque la clé spécifié est null.
-	 * @return
+	 * @throws NullPointerException est généré lorsque la clef est null.
+	 * @return faux ssi il n'y aucun association pour la clef donnée
 	 */
 	public boolean contains(String key) throws NullPointerException {
 		// Déclaration des variables et dictionnaire des données
 		boolean resultat = false; // true ssi une valeur de ce type existe
+		
 		// Module
 		try {
 			if (key != null){
-				for (Pair i : dictionary)
-					if (i.key.toString() == key)
+				for (int i = numberOfElements - 1; i >=0; i--)
+					if (dictionary[i].key == key)
 						resultat = true;
 			}
-			else
+			else //La clef est null
 				throw new NullPointerException();
 		} catch (NullPointerException e1) {
-			System.out.println("La clé spécifié est null!");
+			System.out.println("La clef spécifié est null! Méthode: contains. Contenu de la clef: " + key);
 		}
+		
 		// Retour de la variable résultat.
 		return resultat;
 	}
 
 	@Override
 	/**
-	 * Crée une nouvelle association clé-valeur.
+	 * Crée une nouvelle association clef-valeur.
 	 * 
 	 *
 	 * @param String : symbole lu dans le programme de Luka
 	 * @param Token : la valeur lue dans le programme de Luka
-	 * @throws NullPointerException  : lance l'exception si la clé est null.
+	 * @throws NullPointerException  : lance l'exception si la clef ou la valeur spécifiée est null.
 	 */
 	public void put(String key, Token value) throws NullPointerException {
-		// TODO Auto-generated method stub
+		//Module
 		try {
-			// n
-			if (key != null) {
-				if (dictionary[dictionary.length - 1] != null)
-					for (int i = 0; i < dictionary.length; i++)
-						if (dictionary[i].key == null) {
-							dictionary[i].value = value;
-							dictionary[i].key = key;
-						} else // Dictionnaire est de longueur maximale!
-						{
-							lengthenTheDictionary();
-							put(key, value);
-						}
-			} else
-				// la clé est null
+			if (key != null && value != null) {
+				if (dictionary[dictionary.length - 1].key == null){
+					dictionary[numberOfElements].key = key;
+					dictionary[numberOfElements++].value = value;
+				}
+				else // Dictionnaire est de longueur maximale!
+				{
+					lengthenTheDictionary();
+					put(key, value);
+				}
+			} 
+			else	// la clef ou la valeur est null
 				throw new NullPointerException();
+			
 		} catch (NullPointerException e2) {
-			System.out.println("La clé est null!");
+			System.out.println("La clef est null!  Méthode: put. Contenu de la clef: " + key 
+					+ ". Contenu de la valeur: " + value + ".");
 		}
 
 	}
 
 	@Override
 	/**
-	 * Cette méthode remplace la valeur associé au clé spécifié.
+	 * Cette méthode remplace la valeur associé au clef spécifié.
 	 * Dans le cas d'un conflit, elle remplace la valeur la plus à gauche.
 	 * 
 	 * @param String : symbole lu dans le programme de Luka
-	 * @throws NoSuchElementException : lance l'exception si la clé n'existe pas.
-	 * @throws NullPointerException : lance l'exception lorsque la clé spécifié est null.
+	 * @throws NoSuchElementException : lance l'exception si la clef n'existe pas.
+	 * @throws NullPointerException : lance l'exception lorsque la clef spécifié est null.
 	 */
 	public void replace(String key, Token value) throws NoSuchElementException, NullPointerException {
 		//Déclaration des variables et dictionnaire des données
-		boolean found = false; //Seulement vrai si on trouve la clé à remplacer.
-		//Module
+		boolean found = false; //Seulement vrai si on trouve la clef à remplacer.
+		
+		//Module		
 		try {
 				if (key != null)
-					for (Pair i : dictionary){
-						if(i.key == key){
-							i.value = value;
+					for (int i = numberOfElements-1; i >= 0; i--){
+						if(dictionary[i].key == key){
+							dictionary[i].value = value;
 							found = true;
+							break;
 						}
 					}
-				else //La clé est null.
+				else //La clef est null.
 					throw new NullPointerException();
 				if (!found)//Aucune valeur trouvée afin de remplacer.
-					throw new NoSuchElementException();			
+					throw new NoSuchElementException();	
 		} catch (NoSuchElementException e1) {
-			System.out.println("L'élément demandé n'existaient pas!");
+			System.out.println("L'élément demandé n'existaient pas! Méthode: replace. Contenu de la clef: " + key 
+					+ ". Contenu de la valeur: " + value + ".");
 		}
+		
 		catch (NullPointerException e2){
-			System.out.println("La clé est null!");
+			System.out.println("La clef est null! Méthode: replace. Contenu de la clef: " + key 
+					+ ". Contenu de la valeur: " + value + ".");
 		}
 	}
 
 	@Override
-
 	/**
-	 * Cette méthode permet d'enlevé la valeur associé à une clé.
+	 * Cette méthode permet d'enlevé la valeur associé à une clef.
 	 * Dans le cas d'un conflit, elle enlève la valeur la plus à gauche.
 	 * 
 	 * @param String : symbole lu dans le programme de Luka
-	 * @throws NoSuchElementException : lance l'exception si la clé n'existe pas.
-	 * @throws NullPointerException : lance l'exception lorsque la clé spécifié est null.
-	 * @return 
+	 * @throws NoSuchElementException : lance l'exception si la clef n'existe pas.
+	 * @throws NullPointerException : lance l'exception lorsque la clef spécifié est null.
+	 * @return La valeur du token associé à la clef enlevé.
 	 */
 	public Token remove(String key) throws NoSuchElementException, NullPointerException {
+		//Déclaration des variables et dictionnaire des données
+		Token resultat = null; //La valeur du token associé à la clef à enlevé
+		boolean found = false; // True ssi l'élément est trouvé.
+		//Module
 		try {
-			//TODO: finish remove	
+
+			//Trouve la clef.
+			if (key != null){
+				for (int i =numberOfElements-1; i >= 0 ; i--) //On commence à chercher à l'élément ajouté le plus récemment.
+				{
+					if (dictionary[i].key.equals(key)){
+						resultat =  dictionary[i].value;
+						dictionary[i].value = null; //Essuyage.
+						dictionary[i].key = null;
+						--numberOfElements;
+						organizeStack(); //On enlève l'espace "null" créé, si c'est le cas.
+						found = true;
+						break;
+					}
+				}
+			}
+			else //Alors la clef est null.
+				 throw new NullPointerException();
+			if (!found)
+				throw new NoSuchElementException();
+				
 		} catch (NoSuchElementException e1) {
-			System.out.println("L'élément à enlevé n'existait pas!");
+			System.out.println("L'élément à enlevé n'existait pas! Méthode: remove. "
+					+ "Contenu de la clef: " + key);
 		}
 		catch (NullPointerException e2){
-			System.out.println("La clé est null!");
+			System.out.println("La clef est null! Méthode: remove. Contenu de la clef: " + key);
 		}
-		return null;
+		//Retour de la variable résultat.
+		return resultat;
 	}
+	
 	/**
 	 * Cette classe emmagasine une association String-Token. Elle est utilisé en
-	 * tant que paramètre de la classe pour emmagasiné l'association des
-	 * valeurs.
+	 * tant qu'attribut de la classe pour emmagasiné l'association des valeurs.
 	 */
 	private static class Pair {
 		private Token value;
@@ -195,13 +259,48 @@ public class Dictionary implements Map<String, Token> {
 	 */
 	private void lengthenTheDictionary() {
 		// Déclaration des variables et dictionnaire des données.
-		Pair[] tempDictionary = new Pair[dictionary.length
-				+ SIZE_ADDED_TO_FULL_DICTIONARY];
-		int counter = 0;
+		Pair[] newDictionary = new Pair[dictionary.length //Nouvelle dictionnaire qui sera plus grand que le dernier
+				+ SIZE_ADDED_TO_FULL_DICTIONARY];		  //Et remplacera l'actuel avec les mêmes valeurs.
+		
 		// Module
-		for (Pair i : dictionary)
-			tempDictionary[++counter] = i;
-		// La nouvelle dictionnaire prend la valeur de celle de la classe.
-		dictionary = tempDictionary;
+		//On associe les valeurs du dictionnaire actuel avec la nouvelle
+		for (int i =0; i < dictionary.length; i++)
+			newDictionary[i] = dictionary[i];
+		//On crée les éléments null qui ne sont pas présent dans le vieux dictionnaire
+		for (int i = newDictionary.length - SIZE_ADDED_TO_FULL_DICTIONARY; i < newDictionary.length; i++)
+			newDictionary[i] = new Pair();
+		
+		dictionary = newDictionary;
+	}
+	
+	/**
+	 * Cette méthode réorganise le stack lorsqu'il y a exactement <b> une </b> valeur null
+	 * centré entre deux valeurs non null ou une valeur null au début de la matrice 
+	 * <code> dictionary</code>. 
+	 */
+	private void organizeStack(){ 
+		//Déclaration des variables et dictionnaire des données
+		Pair temp =null; // Valeur temporaire pour faire l'échange.
+		
+		//Module
+		for (int i = 1; i < (dictionary.length -1); i++)
+		{  
+			if (dictionary[0].key == null && dictionary[1].key != null) //Si la première clef est null.
+			{
+				temp = dictionary[1];
+				dictionary[1] = dictionary[0];
+				dictionary[0] = temp;
+			}
+			//On vérifie que l'élément null trouvé n'est pas null des deux coté pour que l'on ne 
+			//change pas les valeurs nulls.
+			if ((dictionary[i].key == null && dictionary[i-1].key != null && dictionary[i+1] !=null))
+			{	//On fait des échanges répétés pour mettre le null à la fin
+				for (int j = i+1; j < dictionary.length; j++){
+					temp = dictionary[j];
+					dictionary[j] = dictionary[j-1];
+					dictionary[j-1] = temp;
+				}
+			}
+		}
 	}
 }
